d763285f0532cb39977a1d827c518e0bcd6204b6
Chris B <chris@chrisbarclay.dev>
2024-09-28 21:15:08 -0700

    Added OAuth for youtube

--- src/app/api/youtube/stream/route.ts
+++ src/app/api/youtube/stream/route.ts
@@ -1,6 +1,7 @@
 // app/api/youtube/stream/route.ts
 import { NextResponse } from 'next/server';
-import ytdl from '@distube/ytdl-core';
+
+import { YtdlCore } from '@ybd-project/ytdl-core';
 
 export async function GET(req: Request) {
   const { searchParams } = new URL(req.url);
@@ -11,14 +12,24 @@ export async function GET(req: Request) {
   }
 
   try {
-    const stream = ytdl(url, {
+
+    const NORMAL_OAUTH2 = new YtdlCore.OAuth2({
+      accessToken: '4eyHJFDykNTWiHXBmaZGhfvHhQGiYyJuZm9TvC8hJpGv5bpxm7A75U8IJz5zir4RiWxXDCRglKoxNwImpBltIbYUu3IXpgtCmizQGBegUaGvAzlG5X3lRoxXLs4EaCgYKAWISARESFQHGX2MidMzLCfXPHv4iKNgTBwl0jQ0187',
+      refreshToken: '1//06tdtu6piLYZJCgYIARAAGAYSNwF-L9IrdJ-EMKtQVzr4k8_zXbDCrtS27g586xeWnqC74FjUJzH31GYfRgP3Ab0xZ64Pznby18E',
+      expiryDate: '2024-09-30T02:40:35.470Z',
+    });
+
+    const ytdl = new YtdlCore({
       filter: 'audioandvideo',
+      oauth2: NORMAL_OAUTH2,
       requestOptions: {
         headers: {
           'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
         }
       }
     });
+
+    const stream = ytdl.download(url);
     console.log('Stream created successfully:', stream); // Debug log
 
     // Create a new ReadableStream from the ytdl stream
